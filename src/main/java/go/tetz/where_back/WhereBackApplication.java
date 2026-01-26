d58989e0ca224d8305b5b03616f3ccae5c30d1fb
kdtTetz <xenosign@naver.com>
2026-01-26 21:54:29 +0900

    Initial commit

---
+++ src/main/java/go/tetz/where_back/WhereBackApplication.java
@@ -0,0 +1,13 @@
+package go.tetz.where_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/go/tetz/where_back/WhereBackApplicationTests.java
@@ -0,0 +1,13 @@
+package go.tetz.where_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
