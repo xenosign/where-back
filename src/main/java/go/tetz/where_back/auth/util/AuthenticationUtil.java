95b0dafd3447d31ad09cafe9c56e5b45186a753b
kdtTetz <xenosign@naver.com>
2026-01-29 19:48:22 +0900

    feat: auth 유틸 클래스 추가 및 properties 설정

--- src/main/java/go/tetz/where_back/auth/util/AuthenticationUtil.java
+++ src/main/java/go/tetz/where_back/auth/util/AuthenticationUtil.java
@@ -1,4 +1,37 @@
 package go.tetz.where_back.auth.util;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class AuthenticationUtil {
+    private final JwtUtil jwtUtil;
+
+    public Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof String) {
+            return Long.valueOf((String) authentication.getPrincipal());
+        }
+        return null;
+    }
+
+    public String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getDetails() instanceof String) {
+            return (String) authentication.getDetails();
+        }
+        return null;
+    }
+
+    public Long getUserIdFromToken(String token) {
+        String userIdStr = jwtUtil.getUserIdFromToken(token);
+        return userIdStr != null ? Long.valueOf(userIdStr) : null;
+    }
+
+    public String getEmailFromToken(String token) {
+        return jwtUtil.getEmailFromToken(token);
+    }
 }
